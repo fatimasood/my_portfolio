7dd1a659a088e6230b5266dba11dfc4960aa54b6
Fatima Masood <99716076+fatimasood@users.noreply.github.com>
2023-06-11 18:24:04 +0000

    personalize my site

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,18 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fatima Masood",
+  title: "",
+  email: "fatimamughal103@gmail.com",
+  gitHub: "https://github.com/fatimasood",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/fatimabintemasood/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
-const primaryColor = "#4E567E";
+const primaryColor = "#5C4033";
 const secondaryColor = "#D2F1E4";
 
 const App = () => {
